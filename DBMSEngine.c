bc65f8b444c5ce342238285d95221c237871c10a
Sejal Gajera <vasanisejal16@gmail.com>
2024-04-26 14:56:22 -0400

    Added the test cases

--- DBMSEngine.c
+++ DBMSEngine.c
@@ -53,11 +53,18 @@ char *rebuild_query_with_function(char *query, char *substring_func)
 
 char *rebuild_query(char *query, char *dbms_type) 
 { 
-    if (strcmp(query, "") ==0|| strcmp(dbms_type, "")==0)
+    char *rebuilt_query="";
+    if (query == NULL || dbms_type == NULL) 
     {
-         printf("DBMS Type and Query can not be empty.");
+         printf("DBMS Type and Query can not be Null.\n");
         return 0;
-    };   
+    }
+    else if (strcmp(query, "") ==0|| strcmp(dbms_type, "")==0) 
+    {
+         printf("DBMS Type and Query can not be empty.\n");
+        return 0;
+    }
+   
     //Get total no of elements in an array
     int  num_database = sizeof(database)/ sizeof (database[0]); 
 
@@ -65,25 +72,52 @@ char *rebuild_query(char *query, char *dbms_type)
         if (strcmp(database[i].dbms_type, dbms_type )==0)
             {
                 // Calling function to replace the appropriate substring function
-               return rebuild_query_with_function(query,  database[i].substring_fun);                
+               rebuilt_query= rebuild_query_with_function(query,  database[i].substring_fun);   
+                if (rebuilt_query != NULL) 
+                {
+                    printf("Rebuilt query: %s\n", rebuilt_query);
+                    free(rebuilt_query);  // Deallocate memory
+                }             
             }
 
   // If given DBMS doesn't exist it array list
-  fprintf(stderr, "Unsupported DBMS: %s\n", dbms_type);
+  if(strcmp(rebuilt_query,"")==0)
+    printf("Unsupported DBMS: %s\n", dbms_type);
   return 0;
 }
 
 int main()
 {
-    char query_def[]  = "select {substringFunction}(columnname, 0 ,4) from tablename";
-    char dbms_type[] = "DBMS2";
+    //Test Case 1: Pass proper value to both the parameters
+    char *query_def  = "select {substringFunction}(columnname, 0 ,4) from tablename";
+    char *dbms_type = "DBMS2";
+
+     rebuild_query(query_def, dbms_type);
+
+     //Test Case 2: Pass query with {substringFunction} to replace at 2 places 
+    query_def = "select {substringFunction}(columnname, 0 ,4) from tablename where {substringFunction}(columnname, 0 ,4)='SAS'";
+    dbms_type = "DBMS2";
    
-    char *rebuilt_query = rebuild_query(query_def, dbms_type);
-    if (rebuilt_query != NULL) 
-    {
-        printf("Rebuilt query: %s\n", rebuilt_query);
-        free(rebuilt_query);  // Deallocate memory
-    }
+    rebuild_query(query_def, dbms_type);
+
+    //Test Case 3: Pass DBMS Type which is not in the list
+    query_def  = "select {substringFunction}(columnname, 0 ,4) from tablename";
+    dbms_type = "DBMS8";
+   
+    rebuild_query(query_def, dbms_type);
+
+    //Test Case 4: pass empty value to both the parameters
+    query_def  = "";
+    dbms_type = "";
+   
+    rebuild_query(query_def, dbms_type);
+
+     //Test Case 5: pass null value in any one parameters
+    query_def  = "select {substringFunction}(columnname, 0 ,4) from tablename";
+    dbms_type = NULL;
+   
+    rebuild_query(query_def, dbms_type);
+   
     return 0;
 }
 
